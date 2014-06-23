60f2b19814df689c417e827d33ab239ae0f01516
Matt Burke <burkemd1+github@gmail.com>
2014-06-23 15:36:14 -0400

    tabs -> spaces

--- src/cursor.js
+++ src/cursor.js
@@ -1,22 +1,22 @@
 var Cursor = function(x, y, field) {
-	this.x = x;
-	this.y = y;
-	this.field = field;
+  this.x = x;
+  this.y = y;
+  this.field = field;
 };
 
 Cursor.prototype.moveLeft = function() {
-	this.tryMoveDelta(-1, 0);
+  this.tryMoveDelta(-1, 0);
 };
 
 Cursor.prototype.moveRight = function() {
-	this.tryMoveDelta(1, 0);
+  this.tryMoveDelta(1, 0);
 };
 
 Cursor.prototype.tryMoveDelta = function(xDelta, yDelta) {
-	if (this.field.isInRange(this.x + xDelta, this.y + yDelta)) {
-		this.x += xDelta;
-		this.y += yDelta;
-	}
+  if (this.field.isInRange(this.x + xDelta, this.y + yDelta)) {
+    this.x += xDelta;
+    this.y += yDelta;
+  }
 };
 
 exportTestSymbol("Cursor", Cursor);
--- test/test.cursor.js
+++ test/test.cursor.js
@@ -2,54 +2,54 @@ var assert = require("assert");
 var LM = require("../dist/landmine.js");
 
 function instance(x, y, width, height) {
-	x = x || 0;
-	y = y || 0;
-	width = width || 10;
-	height = height || 10;
+  x = x || 0;
+  y = y || 0;
+  width = width || 10;
+  height = height || 10;
 
-	return new LM.Cursor(x, y, new LM.MineField({
-		width: width,
-		height: height
-	}));
+  return new LM.Cursor(x, y, new LM.MineField({
+    width: width,
+    height: height
+  }));
 }
 
 describe("Cursor", function() {
-	describe('Constructor', function() {
-		it('sets the x and y', function() {
-			var cursor = new LM.Cursor(1, 2);
-			assert.equal(1, cursor.x);
-			assert.equal(2, cursor.y);
-		});
-	});
+  describe('Constructor', function() {
+    it('sets the x and y', function() {
+      var cursor = new LM.Cursor(1, 2);
+      assert.equal(1, cursor.x);
+      assert.equal(2, cursor.y);
+    });
+  });
 
-	describe('movement', function() {
-		describe("moveLeft", function() {
-			it('decrements the x coordinate', function() {
-				var c = instance(1, 0);
-				c.moveLeft();
-				assert.equal(0, c.x);
-				assert.equal(0, c.y);
-			});
+  describe('movement', function() {
+    describe("moveLeft", function() {
+      it('decrements the x coordinate', function() {
+        var c = instance(1, 0);
+        c.moveLeft();
+        assert.equal(0, c.x);
+        assert.equal(0, c.y);
+      });
 
-			it ('cannot go negative', function() {
-				var c = instance(0,0);
-				c.moveLeft();
-				assert.equal(0, c.x);
-			});
-		});
+      it ('cannot go negative', function() {
+        var c = instance(0,0);
+        c.moveLeft();
+        assert.equal(0, c.x);
+      });
+    });
 
-		describe('moveRight', function() {
-			it('increments the x coordinate', function() {
-				var c = instance();
-				c.moveRight();
-				assert.equal(1, c.x);
-			});
+    describe('moveRight', function() {
+      it('increments the x coordinate', function() {
+        var c = instance();
+        c.moveRight();
+        assert.equal(1, c.x);
+      });
 
-			it('doesnt move past the right edge', function() {
-				var c = instance(9, 0, 10, 10);
-				c.moveRight();
-				assert.equal(9, c.x);
-			});
-		});
-	});
+      it('doesnt move past the right edge', function() {
+        var c = instance(9, 0, 10, 10);
+        c.moveRight();
+        assert.equal(9, c.x);
+      });
+    });
+  });
 });

ba56477447be1767a36073388e1616edf45866e8
Matt Burke <burkemd1+github@gmail.com>
2014-06-24 17:09:58 -0400

    add test folder to jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt){
     },
 
     jshint: {
-      files: ['gruntfile.js', 'src/**/*.js'],
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
         // options here to override JSHint defaults
         globals: {

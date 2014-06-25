b6c74877a152fd07ad7ce6c14fd7f199c8cb7830
Matt Burke <burkemd1+github@gmail.com>
2014-06-25 17:00:23 -0400

    Move math functions to canvas metrics

--- src/artists/canvasmetrics.js
+++ src/artists/canvasmetrics.js
@@ -12,5 +12,44 @@ var CanvasMetrics = function(options) {
   this.locationSize = calculateLocationSize(this.canvasSize, this.fieldSize);
 };
 
+CanvasMetrics.prototype.locationToPoint = function(x, y) {
+  return {
+    x: x * this.locationSize,
+    y: y * this.locationSize
+  };
+};
+
+CanvasMetrics.prototype.locationToCenterPoint = function(x, y) {
+  var upperLeft = this.locationToPoint(x, y);
+  return {
+    x: upperLeft.x + (this.locationSize / 2),
+    y: upperLeft.y + (this.locationSize / 2)
+  };
+};
+
+CanvasMetrics.prototype.locationToScaledOffsetPoint = function(x, y, offsetX, offsetY) {
+  var upperLeft = this.locationToPoint(x, y);
+  return {
+    x : upperLeft.x + offsetX * this.locationSize,
+    y : upperLeft.y + offsetY * this.locationSize
+  };
+};
+
+CanvasMetrics.prototype.applyScaledOffsetsToCenter = function(center, offsetX, offsetY) {
+  return {
+    x: center.x + offsetX * (this.locationSize/2),
+    y: center.y + offsetY * (this.locationSize/2)
+  };
+};
+
+
+CanvasMetrics.prototype.scaleScalar = function(scale) {
+  return this.locationSize * scale;
+};
+
+CanvasMetrics.prototype.locationFontSize = function() {
+  return this.locationSize;
+};
+
 module.exports = CanvasMetrics;
 
---
+++ test/test.canvasmetrics.js
@@ -0,0 +1,58 @@
+var assert = require('assert');
+var CanvasMetrics = require('../src/artists/canvasmetrics');
+
+describe("CanvasMetrics", function() {
+  var metrics;
+  beforeEach(function() {
+    metrics = new CanvasMetrics({
+      canvasSize: { width: 150, height: 100 },
+      fieldSize:  { width: 15,  height: 10  }
+    });
+  });
+
+  describe("locationToPoint", function() {
+
+    it('gives 0, 0 for the top left', function() {
+      var result = metrics.locationToPoint(0,0);
+      assert.equal(0, result.x);
+      assert.equal(0, result.y);
+    });
+
+    it ('gives 140, 90 for the bottom right', function() {
+      var pt = metrics.locationToPoint(14, 9);
+      assert.equal(140, pt.x);
+      assert.equal(90,  pt.y);
+    });
+
+  });
+
+  describe('locationToCenterPoint', function() {
+    it('gives 5, 5 for the uppper left', function() {
+      var pt = metrics.locationToCenterPoint(0, 0);
+      assert.equal(5, pt.x);
+      assert.equal(5, pt.y);
+    });
+
+    it('gives 145, 95 for bottom right', function() {
+      var pt = metrics.locationToCenterPoint(14, 9);
+      assert.equal(145, pt.x);
+      assert.equal(95, pt.y);
+    });
+  });
+
+  describe('locationToScaledOffsetPoint', function() {
+    it('scales 0,0 at 0.2, 0.5 to 2, 5', function() {
+      var pt = metrics.locationToScaledOffsetPoint(0, 0, 0.2, 0.5);
+      assert.equal(2, pt.x);
+      assert.equal(5, pt.y);
+    });
+
+    it ('scales 14,9 at 0.2, 0.5 to 142,95', function() {
+      var pt = metrics.locationToScaledOffsetPoint(14, 9, 0.2, 0.5);
+      assert.equal(142, pt.x);
+      assert.equal(95, pt.y);
+    });
+  });
+
+
+});

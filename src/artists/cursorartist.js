ba32ffb32488b64379d6d75bee79d7174d8e3b10
Matt Burke <burkemd1+github@gmail.com>
2014-06-25 17:06:28 -0400

    use metrics for calculating cursor rect

--- src/artists/cursorartist.js
+++ src/artists/cursorartist.js
@@ -3,14 +3,15 @@ var utils = require('../utils');
 var CursorArtist = function(options) {
   utils.requireOptions(options, 'context', 'metrics');
 
-  this.size = options.metrics.locationSize;
+  this.metrics = options.metrics;
   this.context = options.context;
 };
 
 CursorArtist.prototype.draw = function(x, y) {
   var ctx = this.context;
+  var rect = this.metrics.locationRect(x, y);
   ctx.beginPath();
-  ctx.rect(x * this.size, y * this.size, this.size, this.size);
+  ctx.rect(rect.x, rect.y, rect.width, rect.height);
   ctx.lineWidth = 1;
   ctx.stroke();
 };

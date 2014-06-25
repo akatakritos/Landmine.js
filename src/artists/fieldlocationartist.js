1862e98a96ba849cedc6a50ff2aa2e0d1adf7cfc
Matt Burke <burkemd1+github@gmail.com>
2014-06-25 15:34:28 -0400

    add flag drawing

--- src/artists/fieldlocationartist.js
+++ src/artists/fieldlocationartist.js
@@ -15,6 +15,29 @@ var dirtOffsets = [
   { x: 0.3, y: 0.6 },
   { x: 0.7, y: 0.7 }];
 
+var flagRects = [
+  {
+    color: 'red',
+    x: -0.4666666666666667,
+    y: -0.7333333333333333,
+    width: 0.2,
+    height: 0.2
+  },
+  {
+    color: 'black',
+    x: -0.06666666666666667,
+    y: -0.7333333333333333,
+    width: 0.06666666666666667,
+    height: 0.6666666666666666
+  },
+  {
+    color: 'black',
+    x: -0.4,
+    y: 0.6,
+    width: 0.43333333333333335,
+    height: 0.06666666666666667
+  } ];
+
 var FieldLocationArtist = function(options) {
   utils.requireOptions(options, 'context', 'metrics', 'field');
 
@@ -42,18 +65,6 @@ FieldLocationArtist.prototype.draw = function(location, x, y) {
 
   }
 
-};
-
-FieldLocationArtist.prototype._drawFlag = function(x, y) {
-  var center = utils.squareCenter(x * this.size, y * this.size, this.size);
-  var ctx = this.context;
-
-  ctx.fillStyle    = 'red';
-  ctx.font         = font(this.size);
-  ctx.textAlign    = 'center';
-  ctx.textBaseline = 'middle';
-
-  ctx.fillText("F", center.x, center.y);
 };
 
 FieldLocationArtist.prototype._drawDirt = function(x, y) {
@@ -87,6 +98,28 @@ FieldLocationArtist.prototype._drawCount = function(x, y, count) {
   ctx.fillText(count, center.x, center.y);
 };
 
+
+FieldLocationArtist.prototype._drawFlag = function(x, y) {
+  var center = utils.squareCenter(x * this.size, y * this.size, this.size, this.size);
+  var self = this;
+
+  flagRects.forEach(function(r) {
+    self._fillRect(
+      r.color,
+      center.x + r.x * (self.size/ 2),
+      center.y + r.y * (self.size / 2),
+      r.width * self.size,
+      r.height * self.size);
+  });
+};
+
+FieldLocationArtist.prototype._fillRect = function(color, x, y, w, h) {
+  var ctx = this.context;
+  ctx.beginPath();
+  ctx.rect(x, y, w, h);
+  ctx.fillStyle = color;
+  ctx.fill();
+};
 
 FieldLocationArtist.prototype._drawBorder = function(x, y) {
   var ctx = this.context;

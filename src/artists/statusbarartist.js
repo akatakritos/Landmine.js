08e07e7f64da39fe851fd2b2dfe4dec1679e52d9
Matt Burke <burkemd1+github@gmail.com>
2014-06-26 09:08:21 -0400

    draw other status indicators

--- src/artists/statusbarartist.js
+++ src/artists/statusbarartist.js
@@ -6,19 +6,75 @@ var StatusBarArtist = function(options) {
   this.context = options.context;
 };
 
-StatusBarArtist.prototype.draw = function(timeRemaining) {
+StatusBarArtist.prototype.draw = function(status) {
+  var ctx = this.context;
+  ctx.fillStyle = 'black';
+
+
+  if (typeof status.timeRemaining !== 'undefined') {
+    this._drawTimeRemaining(status.timeRemaining);
+  }
+
+  if (typeof status.score !== 'undefined') {
+    this._drawScore(status.score);
+  }
+
+  if (typeof status.minesRemaining !== 'undefined') {
+    this._drawMinesRemaining(status.minesRemaining);
+  }
+
+  if (typeof status.level !== 'undefined') {
+    this._drawLevel(status.level);
+  }
+};
+
+StatusBarArtist.prototype._drawTimeRemaining = function(timeRemaining) {
+  var corner = this.metrics.statusBarCorner('topleft');
+  var ctx = this.context;
+
+  ctx.font = this.font();
+  ctx.textAlign = 'left';
+  ctx.textBaseline = 'top';
+  ctx.fillText("Time: " + timeRemaining, corner.x, corner.y);
+};
+
+StatusBarArtist.prototype._drawMinesRemaining = function(minesRemaining) {
+  var corner = this.metrics.statusBarCorner("bottomleft");
+  var ctx = this.context;
+
+  ctx.font = this.font();
+  ctx.textAlign = "left";
+  ctx.textBaseline = "bottom";
+  ctx.fillText("Mines: " + minesRemaining, corner.x, corner.y);
+};
+
+StatusBarArtist.prototype._drawLevel = function(level) {
+  var corner = this.metrics.statusBarCorner('topright');
+  var ctx = this.context;
+
+  ctx.font = this.font();
+  ctx.textAlign = 'right';
+  ctx.textBaseline = 'top';
+  ctx.fillText("Level: " + level, corner.x, corner.y);
+};
+
+StatusBarArtist.prototype._drawScore = function(score) {
   var ctx = this.context;
   var pt = this.metrics.statusBarCenter();
-  ctx.fillStyle = 'black';
-  ctx.font = this.font();
+
+  ctx.font = this.scoreFont();
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
-  ctx.fillText("Time: " + timeRemaining, pt.x, pt.y);
+  ctx.fillText(score, pt.x, pt.y);
+};
 
+StatusBarArtist.prototype.scoreFont = function() {
+  var fontSize = this.metrics.statusBarFontSize();
+  return "" + fontSize + "px Arial";
 };
 
 StatusBarArtist.prototype.font = function() {
-  var fontSize = this.metrics.statusBarFontSize();
+  var fontSize = this.metrics.statusBarFontSize() * 0.50;
   return "" + fontSize + "px Arial";
 };
 
